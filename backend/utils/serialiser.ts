a56c8c553d3e71340c6cad02ce608eab95fbdf0c
kimxw <kimaya.wanjari@nuscomputing.com>
2025-05-28 02:47:32 +0800

    Abstracting BigInt to string conversion

---
+++ backend/utils/serialiser.ts
@@ -0,0 +1,15 @@
+export default function serialiseBigInts(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(serialiseBigInts);
+  } else if (typeof obj === 'object' && obj !== null) {
+    const result: any = {};
+    for (const [key, value] of Object.entries(obj)) {
+      result[key] = serialiseBigInts(value);
+    }
+    return result;
+  } else if (typeof obj === 'bigint') {
+    return obj.toString();
+  } else {
+    return obj;
+  }
+}
